8c56d703e7374b3d018fef791980573511d8b2f5
Alexandr Podluzhnyi <sanchoman314@gmail.com>
2021-01-22 14:54:15 +0200

    Add lastname Podluzhnyi

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 echo "Онищенко Богдан, Лектор";
-<<<<<<< HEAD
-
-=======
 echo "Загидуллин Владислав" . PHP_EOL;
 echo "Malyshenko Alexandra, student";
 echo "Dashkovska Anastasia";
 echo "Коновалов Михаил Михайлович" . PHP_EOL;
 echo "NIKITE";
 echo "Podluzhnyi Alexandr";
->>>>>>> develop
+
